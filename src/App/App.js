88d73a45046fcb980d8093dcfe7f5bcc3f3c101e
Khaja Shaik <khajashaik717@gmail.com>
2022-01-26 20:39:10 +0530

    Modified the App component

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { CalenderEvents } from '../CalenderEvents';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='main-app'>
+      <CalenderEvents />
+    </div>
+  );
+}
+
+export default App;
