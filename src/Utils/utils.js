bb30a04c142b5cb70c188b15aaf34f364201bade
Khaja Shaik <khajashaik717@gmail.com>
2022-01-27 17:03:32 +0530

    Configured more time slots

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -1,4 +1,14 @@
 export const timeSlots = [
-    { time: 9, slot: 'AM'},
-    { time: 10, slot: 'AM'}
+    { time: 1, slot: 'AM' },
+    { time: 2, slot: 'AM' },
+    { time: 3, slot: 'AM' },
+    { time: 4, slot: 'AM' },
+    { time: 5, slot: 'AM' },
+    { time: 6, slot: 'AM' },
+    { time: 7, slot: 'AM' },
+    { time: 8, slot: 'AM' },
+    { time: 9, slot: 'AM' },
+    { time: 10, slot: 'AM' },
+    { time: 11, slot: 'AM' },
+    { time: 12, slot: 'PM' },
 ]
